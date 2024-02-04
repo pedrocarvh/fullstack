13d396ed0689f527556554bc30bcc4cb8d225251
Pedro Carvalho <pedrocarvalho.snk@gmail.com>
2024-02-04 17:35:32 -0400

    upadte - question

--- modulos/02-Fundamentos-web-2/02-JS-Módulo-4-javascript-moderno/13-exercicio/app.js
+++ modulos/02-Fundamentos-web-2/02-JS-Módulo-4-javascript-moderno/13-exercicio/app.js
@@ -3,4 +3,19 @@ var media = (...numbers) => {
   return sum / numbers.length
 }
 
-console.log(media(1, 2, 3, 4, 5));
+console.log(`Media Aritm[etica Simples: ${media(1, 2, 3, 4, 5)}`);
+
+
+
+const mediaPonderada = (...entradas) => {
+  const sum = entradas.reduce((accum, { number, peso }) => accum + (number * (peso ?? 1)), 0)
+  const sumPeso = entradas.reduce((accum, entrada) => accum + (entrada.peso??1), 0)
+  return sum / sumPeso
+}
+
+console.log(`Media Ponderada:  ${mediaPonderada(
+  { number: 9, peso: 3 },
+  { number: 7},
+  { number: 10, peso: 1 },
+)}`);
+
